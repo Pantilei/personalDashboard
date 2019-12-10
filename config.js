ba6902c67f7b04e584cd24b324700692881affb5
Pantilei IAnulo <ianulov.pantilei@gmail.com>
2019-12-10 01:38:29 +0000

    preparing to push to herolu

--- config.js
+++ config.js
@@ -1,6 +1,9 @@
 "use strict";
 
 module.exports = {
-  dbConfString: "mongodb://127.0.0.1:27017/code",
+  dbConfString:
+    "mongodb+srv://pantilei:pantilei@dashboard-ypzun.mongodb.net/test?retryWrites=true&w=majority",
   sekretKey: "sekretKey"
 };
+
+//dbConfString: "mongodb://127.0.0.1:27017/code",
