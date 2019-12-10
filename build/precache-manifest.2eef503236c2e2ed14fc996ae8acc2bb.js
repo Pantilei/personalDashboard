69b96bec7fd96ce5ff504eed9e5eecedb7c26bbb
Pantilei IAnulo <ianulov.pantilei@gmail.com>
2019-12-10 02:04:20 +0000

    push to Heroku

---
+++ build/precache-manifest.2eef503236c2e2ed14fc996ae8acc2bb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99747dd51a00e94982027bf180ea436f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a69120bcbfe085181b4",
+    "url": "/static/css/main.3c7146d4.chunk.css"
+  },
+  {
+    "revision": "ba5241c2f426a385456e",
+    "url": "/static/js/2.96011bda.chunk.js"
+  },
+  {
+    "revision": "4a69120bcbfe085181b4",
+    "url": "/static/js/main.e5c9321c.chunk.js"
+  },
+  {
+    "revision": "4c40b94f0748754179e8",
+    "url": "/static/js/runtime-main.bbc06ef5.js"
+  },
+  {
+    "revision": "131eee19c7301c8e594ac6ebab506d93",
+    "url": "/static/media/Background.131eee19.png"
+  },
+  {
+    "revision": "e71a3a20f6416deef004609050e4d09c",
+    "url": "/static/media/home.e71a3a20.PNG"
+  }
+]);
